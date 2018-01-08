26a767a5644d946d568d79271c5ca37be55fa134
CBoom <cboom.trf@gmail.com>
2018-01-08 15:03:11 +0100

    First commit of robot.
    Empty setup of project.

---
+++ MyRobot/MyRobot/src/projects/Robot.java
@@ -0,0 +1,16 @@
+package projects;
+
+public class Robot {
+	public Robot() {
+		super();
+	}
+		
+	public static void main(String[] args) {
+		new Robot().makeItWork();		
+	}
+	
+	private void makeItWork() {
+		
+	}
+
+}
